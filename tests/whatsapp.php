2abf71f65e28f4ae91f84c21b04794ef56861e59
ebbsteinmonster <ebbsteinmonster@gmail.com>
2012-11-19 09:30:08 +0100

    Update tests/whatsapp.php

--- tests/whatsapp.php
+++ tests/whatsapp.php
@@ -14,8 +14,8 @@ function fgets_u($pStdn) {
 }
 
 $nickname = "WhatsAPI Test";
-$sender = 	""; // Mobile number with country code (but without + or 00)
-$imei = 	""; // MAC Address for iOS IMEI for other platform (Android/etc) 
+$sender = 	"4917679395998"; // Mobile number with country code (but without + or 00)
+$imei = 	"359590040667872"; // MAC Address for iOS IMEI for other platform (Android/etc) 
 
 
 $countrycode = substr($sender, 0, 2);
